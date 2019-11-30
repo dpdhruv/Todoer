a9238205a9449805f04507eca22a204baa09004f
dhruv <dhruv_7693@yahoo.co.in>
2019-11-30 19:34:59 +0530

    Fixed production api url issue

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,12 +1,6 @@
 import axios from 'axios';
 
-let base_url='';
-
-if(process.env.NODE_ENV==="development"){
-  base_url="http://localhost:3000/";
-}else{
-  base_url="https://todoeerapi.herokuapp.com/"
-}
+let base_url=process.env.NODE_ENV==='production'?process.env.REACT_APP_DEV_API_URL:process.env.REACT_APP_PROD_API_URL;
 
 
 
